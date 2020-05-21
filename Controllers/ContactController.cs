818216643bd1d1cdcb33f94b6f7836b127148889
Jan3r <janerleonardo@gmail.com>
2020-05-21 16:53:06 -0500

    Modificar y Eliminar

--- Controllers/ContactController.cs
+++ Controllers/ContactController.cs
@@ -40,12 +40,23 @@ namespace WebApiSQLAzure.Controllers
             return Ok ("Agregado correctamente!");
         }
 
+        [HttpPut]
+        public ActionResult Update ([FromBody] Contacts model) 
+        {
+           Contacts updatedContact = model;
+            var selectedElement = contactsContext.ContactSet.Find(updatedContact.Id);
+            selectedElement.Nombre = model.Nombre;
+            selectedElement.Email = model.Email;
+            contactsContext.SaveChanges();
+             return Ok ("Agregado correctamente!");
+        }
+
         [HttpPost]
         public ActionResult Delete ([FromBody] Contacts model) 
         {
-            Contacts item = model;
-            contactsContext.ContactSet.Add(item);
-            contactsContext.SaveChanges ();
+            var selectedElement = contactsContext.ContactSet.Find(model.Id);
+            contactsContext.ContactSet.Remove(selectedElement);
+            contactsContext.SaveChanges();
             return Ok ("Agregado correctamente!");
         }
         
